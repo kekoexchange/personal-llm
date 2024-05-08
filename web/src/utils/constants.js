9332d468c1fd5943e0a26a1ff1cdb2ec77a3a167
Kay Ashaolu <kay@ischool.berkeley.edu>
2024-05-08 09:03:16 -0700

    Fixed auto scrolling issue, added enter key to submit form, aligned buttons properly
    Also refactored constatnts into separate file, On clear intro message is preserved

---
+++ web/src/utils/constants.js
@@ -0,0 +1,8 @@
+export const INTRO_MESSAGE = 
+`******************************
+Welcome to Personal LLM! Ask me anything! Enter in the form below
+******************************\n`;
+
+export const USER_ROLE_NAME = "User";
+export const ASSISTANT_ROLE_NAME = "Assistant";
+
