4d3135e1e3863daa759ab721ba86dbb6b2648c42
Kay Ashaolu <kay@ischool.berkeley.edu>
2024-06-25 14:37:04 -0700

    Adding RAG code

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -1,4 +1,4 @@
 from peewee import SqliteDatabase
 import constants
 
-db_instance = SqliteDatabase(constants.DB_FILE_NAME, pragmas={'foreign_keys': 1})
+db_instance = SqliteDatabase(constants.DB_FILE_PATH, pragmas={'foreign_keys': 1})
